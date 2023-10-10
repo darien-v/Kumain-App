b9c95376691c98fa50eb50cd438dfd908edd9d16
Darien V <darienvazquez.cs@gmail.com>
2023-10-10 19:12:57 -0400

    Name change

--- src/Common/Services/RecipeService.js
+++ src/Common/Services/RecipeService.js
@@ -5,7 +5,7 @@ Recipes.collection = [];
 
 export const getAllRecipes = () => {
     const Recipe = Parse.Object.extend("Recipe");
-    const query = new Parse.Query("Recipe");
+    const query = new Parse.Query(Recipe);
     return query.find().then((results) => {
         console.log("results: ", results);
         return results;
