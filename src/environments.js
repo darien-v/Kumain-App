73df299a8d4295275683097ec4779a3ce823c27a
Jason Brammer <jbrammer@famgenix.com>
2023-10-10 22:21:45 -0400

    change environments

--- src/environments.js
+++ src/environments.js
@@ -1,5 +1,5 @@
 module.exports = {
-    APPLICATION_ID: "BCrUQVkk80pCdeImSXoKXL5ZCtyyEZwbN7mAb11f",
-    JAVASCRIPT_KEY: "4wPYRKbpTJeCdmFNaS31AiQZ8344aaYubk6Uo8VW",
+    APPLICATION_ID: "XZt9g6UylMa1O9EGbi6cDF9fhCRLPthCYQH2ujVP",
+    JAVASCRIPT_KEY: "LAYD7KSdaJpZumUDuPe4GDpdXsyjFVpDDl26mpmp",
     SERVER_URL: "https://parseapi.back4app.com"
   };
