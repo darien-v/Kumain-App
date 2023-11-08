ef3e78d35aa0dc6aa9347e97a39c14cb3fe2acbc
Jason Brammer <jbrammer@famgenix.com>
2023-11-08 14:00:27 -0500

    Login/Signup functionality

---
+++ src/Components/Auth/AuthLogin.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { checkUser, doUserLogIn } from "../../Common/Services/AuthService.js";
+import AuthForm from "./AuthForm";
+import { useNavigate } from "react-router-dom";
+
+const AuthLogin = () => {
+  const [currentUser, setCurrentUser] = useState({
+	email: "",
+	password: ""
+  });
+
+  const navigate = useNavigate();
+
+  const [add, setAdd] = useState(false);
+
+  useEffect(() => {
+	if (checkUser()) {
+		alert("You are already logged in");
+		navigate("/");
+	}
+  }, [navigate]);
+
+  useEffect(() => {
+    if (currentUser && add) {
+      doUserLogIn(currentUser).then((userLoggedIn) => {
+        if (userLoggedIn) {
+          alert(
+            `${userLoggedIn.get("firstName")}, you successfully logged in!`
+          );
+		  // redirecting to the protected route here throws a True is not defined error maybe something wrong with Protected Route
+          // navigate(`/${userLoggedIn.get("username")}/mycookbook`);
+		  navigate("/");
+        }
+        // redirect user to main app
+        setAdd(false);
+      });
+    }
+  }, [navigate, currentUser, add]);
+
+  const onChangeHandler = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    const { name, value: newValue } = e.target;
+    console.log(newValue);
+
+    setCurrentUser({
+      ...currentUser,
+      [name]: newValue
+    });
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log("submitted: ", e.target);
+    setAdd(true);
+  };
+
+  return (
+    <div>
+      <AuthForm
+        user={currentUser}
+        onChange={onChangeHandler}
+        onSubmit={onSubmitHandler}
+        isRegister={false}
+      />
+    </div>
+  );
+};
+
+export default AuthLogin;
---
+++ src/Components/Auth/AuthRegister.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import { createUser } from "../../Common/Services/AuthService.js";
+import AuthForm from "./AuthForm";
+import { useNavigate } from "react-router-dom";
+
+const AuthRegister = () => {
+  const [newUser, setNewUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  });
+
+  // flag is the state to watch for add/remove updates
+  const [add, setAdd] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (newUser && add) {
+      createUser(newUser).then((userCreated) => {
+        if (userCreated) {
+          alert(
+            `${userCreated.get("firstName")}, you successfully registered!`
+          );
+          // we should navigate to authenticated user page because when registering and then going to login
+          // it throws an error saying you are already logged in because the cookies of the registered user get
+          // added to the localStorage. You have to manually delete local Storage and then login after register
+          navigate("/");
+        }
+        setAdd(false);
+      });
+    }
+  }, [newUser, add]);
+
+  const onChangeHandler = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    const { name, value: newValue } = e.target;
+    console.log(newValue);
+    setNewUser({ ...newUser, [name]: newValue });
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log("submitted: ", e.target);
+    setAdd(true);
+  };
+
+  return (
+    <div>
+      <AuthForm
+        user={newUser}
+        onChange={onChangeHandler}
+        onSubmit={onSubmitHandler}
+        isRegister={true}
+      />
+    </div>
+  );
+};
+
+export default AuthRegister;
