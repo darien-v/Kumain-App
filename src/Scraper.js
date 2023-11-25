fddf69ee319b850e8ef84bbedbf3f725bbf641bd
Darien V <darienvazquez.cs@gmail.com>
2023-11-25 13:30:39 -0500

    scrapes up to individual cuisine page

--- src/Scraper.js
+++ src/Scraper.js
@@ -1,9 +1,60 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const pretty = require("pretty")
+const parse = require("parse")
 //this url is the root page from which we navigate to all recipes for each cuisine type
-//
 const rootARURL = "https://www.allrecipes.com/cuisine-a-z-6740455"
+//setting an artificial limit on how many recipes can be scraped on one launch of the web app
+const limit = 10
+
+//this function assumes the string is either of the "# mins" or "# hr # mins" form
+function convertMinutes(durationString){
+    let totalTime = 0
+    const splitString = durationString.split(" ")
+    let index = 0
+    if (splitString.length > 2){
+        totalTime += parseInt(splitString[index])*60
+        index += 2
+    }
+    totalTime += parseInt(splitString[index])
+    return totalTime
+}
+
+//this should return if we were able to successfully scrape, the name, cook time, prep time, servings, and ingredients for a recipe
+async function scrapeRecipe(link){
+    try{
+        const {data} = await axios.get()
+        const $ = cheerio.load(data)
+        const recipeName = $('[id="article-heading_1-0"]').text()  
+        //on allRecipes, the prep and cook time are always first and second respectively
+        const prepTime = convertMinutes($('div.mntl-recipe-details__value:first').text())
+        const cookTime = convertMinutes($('div.mntl-recipe-details__value:second').text())
+        //serving size usually third, but if additional time is involved, it could be later 
+        const servingSize = parseInt($('div.mntl-recipe-details__item:contains("Servings:").mntl-recipe-details__value').text())
+        const ingredients = [...$('ul.mntl-structured-ingredients__list li span [data-ingredient-name=true]')].map(e => $(e).text())
+        return true, recipeName, prepTime, cookTime, servingSize, ingredients
+    } catch (err){
+        //currently returns error
+        console.log("Couldn't retrive html")
+        console.log(err)
+        return false, null, null, null, null, null
+    }
+}
+
+async function scrapeCuisinePage(categories){
+    try{
+        const {data} = await axios.get(categories.link)
+        const $ = cheerio.load(data)
+        const topRecipes = [...$("a.comp.card--image-top.mntl-card-list-items.mntl-document-card.mntl-card.card.card--no-image")].map(e => $(e).attr("href"))
+        const remainingRecipes = [...$("a.comp.mntl-card-list-items.mntl-document-card.mntl-card.card.card--no-image")].map(e => $(e).attr("href"))
+        const allRecipes = topRecipes.concat(remainingRecipes)
+        console.log(allRecipes)
+        console.log("Recipe Count: ", allRecipes.length)
+        console.log(scrapeRecipe(allRecipes[0]))
+    } catch (err){
+        console.log(err)
+    }
+}
 
 async function scrapeAllRecipes(){
     try{
@@ -17,6 +68,7 @@ async function scrapeAllRecipes(){
         }))
         console.log(categoriesLinks)
         //from here we grab the links found on each page, set to limit to some arbitrary amount
+        scrapeCuisinePage(categoriesLinks[0])
     } catch (err){
         console.log(err)
     }
