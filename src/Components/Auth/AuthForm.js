4a270048f1984b46fe03332c40da6a6534fbbec5
Jason Brammer <jbrammer@famgenix.com>
2023-12-04 16:22:07 -0500

    css register/login

--- src/Components/Auth/AuthForm.js
+++ src/Components/Auth/AuthForm.js
@@ -3,87 +3,87 @@ import { Link } from "react-router-dom";
 import "./Auth.css"
 
 const AuthForm = ({ user, onChange, onSubmit, isRegister }) => {
-  const white_background = {
-    backgroundColor: "white"
-  }
   return (
-    <div className="auth">
-      {isRegister && (
-        <div className="message">
+    <div>
+      <div className="image-content">
+        <div className="first">
+        </div>
+        <div className="second">
+        </div>
+      </div>
+      <div className="third">
+       {isRegister && (
           <h1>Create Your Account!</h1>
-        </div>
-      )}
-      {!isRegister && (
-        <div className="message">
+        )}
+        {!isRegister && (
           <h1>Log Into Your Account!</h1>
-        </div>
-      )}
-      <hr style={white_background} />
-      <form onSubmit={onSubmit}>
-        {isRegister && (
-          <div className="input">
-            <label>First Name</label>
+        )}
+        <form onSubmit={onSubmit}>
+          {isRegister && (
+            <div className="input">
+              <label>First Name</label>
+              <br />
+              <input
+                type="text"
+                value={user.firstName}
+                onChange={onChange}
+                name="firstName"
+                placeholder="first name"
+                required
+              />
+            </div>
+          )}
+          {isRegister && (
+            <div  className="input" >
+              <label>Last Name</label>
+              <br />
+              <input
+                type="text"
+                value={user.lastName}
+                onChange={onChange}
+                name="lastName"
+                placeholder="last name"
+                required
+              />
+            </div>
+          )}
+          <div  className="input">
+            <label>Email</label>
             <br />
             <input
-              type="text"
-              value={user.firstName}
+              type="email"
+              value={user.email}
               onChange={onChange}
-              name="firstName"
-              placeholder="first name"
+              name="email"
+              placeholder="email"
               required
             />
           </div>
-        )}
-        {isRegister && (
-          <div  className="input" >
-            <label>Last Name</label>
+          <div  className="input">
+            <label>Password</label>
             <br />
             <input
-              type="text"
-              value={user.lastName}
+              type="password"
+              value={user.password}
               onChange={onChange}
-              name="lastName"
-              placeholder="last name"
+              name="password"
+              placeholder="password"
+              min="0"
               required
             />
           </div>
-        )}
-        <div  className="input">
-          <label>Email</label>
-          <br />
-          <input
-            type="email"
-            value={user.email}
-            onChange={onChange}
-            name="email"
-            placeholder="email"
-            required
-          />
-        </div>
-        <div  className="input">
-          <label>Password</label>
-          <br />
-          <input
-            type="password"
-            value={user.password}
-            onChange={onChange}
-            name="password"
-            placeholder="password"
-            min="0"
-            required
-          />
-        </div>
-        <div className="submit">
-          <button type="submit" onSubmit={onSubmit}>
-            {isRegister ? "Register" : "Log In"}
-          </button>
-        </div>
-        {isRegister && (
-          <div className="login">
-            <p>Already have an account? <Link to="/login" className="link">Log In</Link></p>
+          <div className="submit">
+            <button type="submit" onSubmit={onSubmit}>
+              {isRegister ? "Register" : "Log In"}
+            </button>
           </div>
-        )}
-      </form>
+          {isRegister && (
+            <div className="login">
+              <p>Already have an account? <Link to="/login" className="link">Log In</Link></p>
+            </div>
+          )}
+        </form>
+      </div>
     </div>
   );
 };
