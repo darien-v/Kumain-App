9fab95d63dfeadbd79d2e3f5b662f9040c6e567d
Jason Brammer <jbrammer@famgenix.com>
2023-12-07 11:33:39 -0500

    search

--- src/Components/Search/SearchForm.js
+++ src/Components/Search/SearchForm.js
@@ -1,47 +1,74 @@
-import Select from 'react-select'
+import Select from 'react-select';
 import { RangeSlider } from "react-double-range-slider";
+import { useState } from 'react';
 import ReactSlider from 'react-slider'
 import "react-double-range-slider/dist/cjs/index.css";
+import "./Search.css"
 //assume ingredients is a list of unique ingredients taken from backend
 //minCook and maxCook also taken from backend
 //same with minPrep and maxPrep
 const SearchForm = ({ingredients, minCook, maxCook, minPrep, maxPrep, minServings, maxServings, cuisineList}) => {
+    const [cookTime, setCookTime] = useState(minCook);
+    const [prepTime, setPrepTime] = useState(minPrep);
+    const [servings, setServings] =useState(minServings);
+
+    const handleValueChange = (e) => {
+        console.log(e.target)
+    }
+
     return (
-        <div>
+        <div className="search">
             <h1>Search for recipes</h1>
-            <h3>Looking for recipes? Our handy search tool can find recipes from the ingredients you have!</h3>
-            <h2>Ingredients</h2>
-            <Select 
+            <h5>Looking for recipes? Our handy search tool can find recipes from the ingredients you have!</h5>
+            <h4>Ingredients</h4>
+            <Select
                 options = {ingredients}
                 isMulti
                 isClearable = {true}
                 isSearchable = {true}
             />
-            <h2>Cuisine Types</h2>
+            <h4>Cuisine Types</h4>
             <Select 
-                options = {cuisneList}
+                options = {cuisineList}
                 isMulti
                 isClearable = {true}
                 isSearchable = {true}
             />
-            <h2>Prep Time</h2>
-            <RangeSlider
-                value = {{min: minPrep, max: maxPrep}}
+            <h4>Prep Time</h4>
+            <div className="slidecontainer">
+                <input type="range" min="1" max="100" value="50"/>
+            </div>
+            {/* <RangeSlider
+                onValueChanged = {handleValueChange}
+                start={minPrep}
+                end={maxPrep}
                 tooltipPosition='over'
                 tooltipVisibility='always'
-            />
-            <h2>Cook Time</h2>
-            <RangeSlider
-                value = {{min: minCook, max: maxCook}}
+            /> */}
+            <h4>Cook Time</h4>
+            <div className="slidecontainer">
+                <input type="range" min="1" max="100" value="50" />
+            </div>
+            {/* <RangeSlider
+                onValueChanged = {handleValueChange}
+                start={minCook}
+                end={maxCook}
                 tooltipPosition='over'
                 tooltipVisibility='always'
-            />
-            <h2>Serving Size</h2>
-            <RangeSlider
-                value = {{min: minServings, max: maxServings}}
+            /> */}
+            <h4>Serving Size</h4>
+            <div className="slidecontainer">
+                <input type="range" min="1" max="100" value="50" />
+            </div>
+            {/* <RangeSlider
+                onValueChanged = {handleValueChange}
+                start={minServings}
+                end={maxServings}
                 tooltipPosition='over'
                 tooltipVisibility='always'
-            />
+            /> */}
         </div>
     )
 }
+
+export default SearchForm;
